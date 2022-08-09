f1cf8af7b3046246fd1be5152edeb46aae78ad9f
petersulyok <peter@sulyok.net>
2022-08-09 16:04:12 +0200

    A real hd_name= configuration value added to smfc.conf at installation time.

--- install.sh
+++ install.sh
@@ -27,4 +27,11 @@ cp ./src/smfc /etc/default/
 cp ./src/smfc.service /etc/systemd/system/
 chown root.root "$TARGET_DIR/smfc.py" "$TARGET_DIR/smfc.conf" /etc/default/smfc /etc/systemd/system/smfc.service
 
+# Generate a real hd_names= entry in the new 'smfc.conf'.
+hd_name = $(ls -l /dev/disk/by-id/|grep .*ata-.*sda$|cut -d ' ' -f 10)
+if [ -n $hd_name ];
+then
+  sed -i 's|hd_names=|hd_names=$hd_name|g' "$TARGET_DIR/smfc.conf"
+fi
+
 echo "Installation finished successfully."
