9ad8b241fd6e1f01b3de02fe0a3a773eaa4dc5b6
petersulyok <peter@sulyok.net>
2023-11-28 17:31:31 +0100

    New: version is specified in command line argument.

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+#
+#   docker-build.sh (C) 2023, Peter Sulyok
+#   This script will build `smfc` image.
+#
 
-version="3.3.0"
-
+if [ "$1" == "" ]; then
+   echo "Usage: docker-build.sh version"
+   echo "Example: docker-build.sh 3.4.0"
+   exit -1
+fi
+version=$1
 docker build . -t petersulyok/smfc:$version -t petersulyok/smfc:latest --label "org.opencontainers.image.version=$version" -f Dockerfile
+#docker push  petersulyok/smfc:$version
+
