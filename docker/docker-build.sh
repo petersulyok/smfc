32b927e3280ea9d79283c7b490d632c8e71463e6
petersulyok <peter@sulyok.net>
2024-02-22 09:26:32 +0100

    Fix:the latest tag added to the docker image properly.

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #
-#   docker-build.sh (C) 2023, Peter Sulyok
+#   docker-build.sh (C) 2023-2024, Peter Sulyok
 #   This script will build `smfc` image.
 #
-
-if [ "$1" == "" ]; then
+set -e
+if [ "$1" == "" ];
+then
    echo "Usage: docker-build.sh version"
    echo "Example: docker-build.sh 3.4.0"
-   exit -1
+   exit 1
 fi
 version=$1
-docker build . -t petersulyok/smfc:$version -t petersulyok/smfc:latest --label "org.opencontainers.image.version=$version" -f Dockerfile
-#docker push  petersulyok/smfc:$version
-
+docker build . -t petersulyok/smfc:$version --label "org.opencontainers.image.version=$version" -f Dockerfile
+docker tag petersulyok/smfc:$version petersulyok/smfc:latest
+docker push petersulyok/smfc
