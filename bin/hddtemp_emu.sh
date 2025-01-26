7b4e5cfed123d5f0542061f7ca206160caf3c83f
petersulyok <peter@sulyok.net>
2025-01-26 19:33:08 +0100

    Fix: simplified return value checking.

--- bin/hddtemp_emu.sh
+++ bin/hddtemp_emu.sh
@@ -2,7 +2,7 @@
 #
 #   hddtemp_emu.sh (C) 2025, Peter Sulyok
 #   This script will emulate hddtemp command (with the help of `smartctl`).
-#   The expected use (based on the way how smfc.py is calling hddtemp):
+#   The expected use (based on the way how `smfc.py` is calling `hddtemp`):
 #
 #       $ hddtemp_emu.sh -q -n /dev/sda
 #       27
@@ -24,8 +24,7 @@ fi
 
 # Read disk temperature with `smartctl` command.
 hdd_temp=$(smartctl -a $3|grep -m 1 Temp|tr -s " "|cut -d" " -f 10)
-rv=$?
 
 # Print the temperature out.
 echo "$hdd_temp"
-exit $rv
+exit 0
