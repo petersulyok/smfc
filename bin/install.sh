073dc4f631f19cf7a6a2bdb6c5101f3a4e8c31fa
petersulyok <peter@sulyok.net>
2025-09-06 19:42:35 +0200

    New: local installation added again to `./bin/install.sh`

--- bin/install.sh
+++ bin/install.sh
@@ -5,13 +5,17 @@
 #
 set -e
 
+
 function print_help() {
-  echo "usage: $(basename $0) [-h|--help] [-k|--keep-config] [-v|--verbose]"
+  echo "usage: $(basename $0) [-h|--help] [-k|--keep-config] [-l|--local] [-v|--verbose]"
   echo "           -h, --help         help text"
   echo "           -k, --keep-config  keep original configuration file"
+  echo "           -l, --local        installation from a local git repository"
   echo "           -v, --verbose      verbose output"
+
   exit 0
 }
+
 
 function verbose_echo() {
   if [ -n "${VERBOSE}" ]; then
@@ -19,10 +23,28 @@ function verbose_echo() {
   fi
 }
 
+
+function new_configuration() {
+  # Create the configuration target folder if it does not exist.
+  if [ ! -d "$TARGET_DIR" ]; then
+    mkdir -p $TARGET_DIR
+    verbose_echo "$TARGET_DIR folder created."
+  fi
+  # Backup the old configuration file if required.
+  if [ -z "${KEEP_CONFIG}" ]; then
+    if [ -f "$TARGET_DIR/smfc.conf" ]; then
+      cp "$TARGET_DIR/smfc.conf" "$TARGET_DIR/smfc.conf.$POST_TAG"
+      verbose_echo "Original configuration file saved ($TARGET_DIR/smfc.conf.$POST_TAG)."
+    fi
+    # Execute installation command
+    $1
+    verbose_echo "New configuration file installed ($TARGET_DIR/smfc.conf)."
+  fi
+}
+
+
 # Postfix extension for backup files.
 POST_TAG=$(date +%4Y%m%d_%H%M%S)
-# GitHUb URL
-GITHUB_URL="https://raw.githubusercontent.com/petersulyok/smfc/refs/heads/main"
 
 # Parsing of command line arguments
 while [[ $# -gt 0 ]]; do
@@ -32,6 +54,10 @@ while [[ $# -gt 0 ]]; do
       ;;
     -k|--keep-config)
       KEEP_CONFIG="yes"
+      shift # past value
+      ;;
+    -l|--local)
+      LOCAL_INSTALL="yes"
       shift # past value
       ;;
     -v|--verbose)
@@ -53,41 +79,73 @@ if [[ $EUID -ne 0 ]]; then
 fi
 verbose_echo "Root privilege granted."
 
-# Red the lastest smfc version from GitHub.
-LATEST_VERSION=$(curl --silent "$GITHUB_URL/pyproject.toml"|grep "version = "|cut -d "\"" -f2)
-verbose_echo "Latest smfc version from GitHub: $LATEST_VERSION"
 
-# Install latest `smfc` package from Pypi.org
-pip install -q --prefix=/usr smfc==$LATEST_VERSION
-verbose_echo "pip installed smfc==$LATEST_VERSION."
+# CASE 1: Local installation from the current folder.
+if [ "$LOCAL_INSTALL" = "YES" ]; then
 
-# Install configuration file.
-TARGET_DIR=/etc/smfc
-# Create the target folder if does not exist.
-if [ ! -d "$TARGET_DIR" ]; then
-  mkdir -p $TARGET_DIR
-  verbose_echo "$TARGET_DIR folder created."
-fi
-# Backup the old configuration file and install the new one.
-if [ -z "${KEEP_CONFIG}" ]; then
-  if [ -f "$TARGET_DIR/smfc.conf" ]; then
-    cp "$TARGET_DIR/smfc.conf" "$TARGET_DIR/smfc.conf.$POST_TAG"
-    verbose_echo "Originial configuration file saved ($TARGET_DIR/smfc.conf.$POST_TAG)."
-  fi
-  curl --silent -o "$TARGET_DIR/smfc.conf" "$GITHUB_URL/config/smfc.conf"
+  # Display the installation type.
+  verbose_echo "Local installation from current folder."
+
+  # Read smfc version from the current folder.
+  SMFC_VERSION=$(grep "version = " ./pyproject.toml|cut -d "\"" -f2)
+  verbose_echo "smfc version: $SMFC_VERSION."
+
+  # Install smfc package from `./dist` folder
+  pip install -q --prefix=/usr "./dist/smfc-$SMFC_VERSION.tar.gz"
+  verbose_echo "pip installed smfc==$SMFC_VERSION."
+
+  # Install configuration file.
+  TARGET_DIR=/etc/smfc
+  new_configuration "cp ./config/smfc.conf $TARGET_DIR/smfc.conf"
   verbose_echo "New configuration file installed ($TARGET_DIR/smfc.conf)."
+
+  # Install smfc files.
+  cp -f ./config/smfc /etc/default/smfc
+  cp -f ./config/smfc.service /etc/systemd/system/smfc.service
+  mkdir -p /usr/local/share/man/man1
+  cp -f ./doc/smfc.1 /usr/local/share/man/man1/smfc.1
+  chown root:root "$TARGET_DIR/smfc.conf" /etc/default/smfc /etc/systemd/system/smfc.service /usr/local/share/man/man1/smfc.1
+
+
+# CASE 2: Remote installation from GitHub and pypi.
+else
+
+  # Display the installation type.
+  verbose_echo "Remote installation from GitHub."
+
+  # GitHUb URL
+  GITHUB_URL="https://raw.githubusercontent.com/petersulyok/smfc/refs/heads/main"
+
+  # Read the smfc version from GitHub.
+  SMFC_VERSION=$(curl --silent "$GITHUB_URL/pyproject.toml"|grep "version = "|cut -d "\"" -f2)
+  verbose_echo "smfc version from GitHub: $SMFC_VERSION"
+
+  # Install smfc package from Pypi.org
+  pip install -q --prefix=/usr smfc==$SMFC_VERSION
+  verbose_echo "pip installed smfc==$SMFC_VERSION."
+
+  # Install configuration file.
+  TARGET_DIR=/etc/smfc
+  new_configuration "curl --silent -o $TARGET_DIR/smfc.conf $GITHUB_URL/config/smfc.conf"
+
+  # Install smfc files.
+  curl --silent -o "/etc/default/smfc" "$GITHUB_URL/config/smfc"
+  curl --silent -o "/etc/systemd/system/smfc.service" "$GITHUB_URL/config/smfc.service"
+  mkdir -p /usr/local/share/man/man1
+  curl --silent -o "/usr/local/share/man/man1/smfc.1" "$GITHUB_URL/doc/smfc.1"
+
 fi
 
-# Install smfc files.
-curl --silent -o "/etc/default/smfc" "$GITHUB_URL/config/smfc"
-curl --silent -o "/etc/systemd/system/smfc.service" "$GITHUB_URL/config/smfc.service"
-mkdir -p /usr/local/share/man/man1
-curl --silent -o "/usr/local/share/man/man1/smfc.1" "$GITHUB_URL/doc/smfc.1"
-gzip "/usr/local/share/man/man1/smfc.1"
+gzip -f /usr/local/share/man/man1/smfc.1
 mandb > /dev/null 2>&1
+chown root:root \
+  "$TARGET_DIR/smfc.conf" \
+  /etc/default/smfc \
+  /etc/systemd/system/smfc.service \
+  /usr/local/share/man/man1/smfc.1.gz
 verbose_echo "smfc files have been installed."
 
-# Collect all disk names for `hd_names=` parameter in case of a 'smfc.conf' file.
+# Collect all disk names for `hd_names=` parameter in 'smfc.conf' file.
 if [ -z "${KEEP_CONFIG}" ]; then
   hd_list=$(ls /dev/disk/by-id/|grep -v -E ".*-part.$"|grep -v -E ".*_1$")
   if [ -n "$hd_list" ]; then
@@ -95,9 +153,9 @@ if [ -z "${KEEP_CONFIG}" ]; then
     for hl in $hd_list; do
       hd_names+="/dev/disk/by-id/$hl\n\t"
     done
-    sed -i "s|hd_names=|hd_names=$hd_names|g" "$TARGET_DIR/smfc.conf"
-    verbose_echo "HDD names are listed in the new configuration file."
+    sed -i "s|^hd_names=|hd_names=$hd_names|g" "$TARGET_DIR/smfc.conf"
+    verbose_echo "HDD names are listed in the new configuration file (please edit!)."
   fi
 fi
 
-verbose_echo "smfc installation finished."
+verbose_echo "smfc installation finished successfully."
